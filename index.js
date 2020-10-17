6f6adbf17fb576669f189eaf83caadb586ac9b67
Guilherme Santiago <guilherme.santiago95@gmail.com>
2020-10-17 13:35:08 -0300

    add form

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
+require('dotenv').config()
 const express = require('express')
+const webPush = require('web-push')
 const app = express()
-const port = 3003
+const port = process.env.PORT || 3003
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
+webPush.setVapidDetails(
+  "mailto:guilhermess@pbtech.net.br",
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
+)
+
+app.use(express.json())
+
+app.get('/', (_req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/send', async (req, res) => {
+  try {
+    const { title, body, url, subscription } = req.body
+
+    const payload = JSON.stringify({
+      title,
+      body,
+      url
+    })
+
+    const response = await webPush.sendNotification(subscription, payload, { TTL: 60 })
+
+    res.json(response)
+  } catch (error) {
+    console.error(error)
+    res.status(500)
+    res.json({ error: error.message })
+  }
 })
 
 app.listen(port, () => {
